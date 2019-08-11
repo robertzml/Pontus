83ee306026f606bc6874dfb358807724ed9b3b6c
robertzml <robertzml@126.com>
2019-08-12 01:00:59 +0800

    import axios. add customer table.
    Signed-off-by: robertzml <robertzml@126.com>

--- src/views/Customer/Index.vue
+++ src/views/Customer/Index.vue
@@ -2,14 +2,10 @@
   <v-layout wrap>
     <v-flex xs12 md12>
       <v-card class="mx-auto">
-        <v-card-title>客户管理</v-card-title>
+        <v-card-title class="cyan">客户管理</v-card-title>
         <v-card-text>
           I'm card text
-          <v-combobox
-            v-model="select"
-            :items="items"
-            label="Select a favorite activity or create a new one"
-          ></v-combobox>
+          <v-combobox v-model="select" :items="items" label="Select a favorite activity or create a new one"></v-combobox>
         </v-card-text>
 
         <v-card-actions>
@@ -23,8 +19,17 @@
     <customer-edit ref="customerMod"></customer-edit>
 
     <v-flex xs12>
-      <v-card dark color="primary">
-        <v-card-text class="px-0">12</v-card-text>
+      <v-card dark>
+        <v-card-title class="orange">客户列表</v-card-title>
+        <v-card-text class="px-0">
+          <v-data-table :headers="headers" :items="customerData" :items-per-page="10">\
+            <template v-slot:item.action="{ item }">
+              <v-icon small @click="editItem(item)">
+                edit
+              </v-icon>
+            </template>
+          </v-data-table>
+        </v-card-text>
       </v-card>
     </v-flex>
     <v-flex v-for="i in 2" :key="`6${i}`" xs6>
@@ -46,12 +51,33 @@ export default {
   data: () => ({
     select: 'Programming',
     items: ['Programming', 'Design', 'Vue', 'Vuetify'],
-    dialog: false
+    dialog: false,
+    customerData: [],
+    headers: [
+      { text: '编号', value: 'number', align: 'left' },
+      { text: '客户名称', value: 'name' },
+      { text: '地址', value: 'address' },
+      { text: '电话', value: 'telephone' },
+      { text: '联系人', value: 'contact' },
+      { text: '操作', value: 'action', sortable: false }
+    ]
   }),
   methods: {
     openMod() {
       this.$refs.customerMod.init(0)
     }
+  },
+  mounted: function() {
+    console.log('customer')
+    let vm = this
+    this.axios
+      .get('/customer/list')
+      .then(function(res) {
+        vm.customerData = res.data
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
   }
 }
 </script>
