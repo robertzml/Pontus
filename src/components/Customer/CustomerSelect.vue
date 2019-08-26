39b5f02ad7ffc0842c62269c1a1692a143113adb
robertzml <robertzml@126.com>
2019-08-27 00:18:02 +0800

    use tr, td in auto complete.
    Signed-off-by: robertzml <robertzml@126.com>

--- src/components/Customer/CustomerSelect.vue
+++ src/components/Customer/CustomerSelect.vue
@@ -1,5 +1,19 @@
 <template>
   <v-autocomplete label="请选择客户" prepend-icon="people" :items="customerData" item-value="id" item-text="name">
+    <template v-slot:prepend-item>
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Calories</th>
+        </tr>
+      </thead>
+    </template>
+    <template v-slot:item="data">
+      <tr>
+        <td v-text="data.item.name"></td>
+        <td v-text="data.item.number"></td>
+      </tr>
+    </template>
   </v-autocomplete>
 </template>
 
