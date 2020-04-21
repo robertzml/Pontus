61d75d4297140eee3fc8fe96b380898359ea61c5
robertzml <robertzml@126.com>
2020-04-21 21:48:34 +0800

    use component in carry details.
    Signed-off-by: robertzml <robertzml@126.com>

--- src/views/CarryIn/Details.vue
+++ src/views/CarryIn/Details.vue
@@ -4,83 +4,7 @@
       搬运入库任务信息
     </v-card-title>
     <v-card-text>
-      <v-row dense>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="任务码" v-model="carryInInfo.taskCode" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="搬运类型" :value="$util.carryInTaskType(carryInInfo.type)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="仓位码" v-model="carryInInfo.positionNumber" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="货架码" v-model="carryInInfo.shelfCode" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="托盘码" v-model="carryInInfo.trayCode" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="客户名称" v-model="carryInInfo.customerName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="合同名称" v-model="carryInInfo.contractName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="货品名称" v-model="carryInInfo.cargoName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="货品类别" :value="`${carryInInfo.categoryNumber} - ${carryInInfo.categoryName}`" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="搬运数量" v-model="carryInInfo.moveCount" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="搬运重量" v-model="carryInInfo.moveWeight" suffix="吨" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="规格" v-model="carryInInfo.specification" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="单位重量" v-model="carryInInfo.unitWeight" suffix="千克" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="库存入库时间" :value="$util.displayDate(carryInInfo.inTime)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="库存出库时间" :value="$util.displayDate(carryInInfo.outTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="清点人" v-model="carryInInfo.checkUserName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="清点时间" :value="$util.displayDateTime(carryInInfo.checkTime)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="接单人" v-model="carryInInfo.receiveUserName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="接单时间" :value="$util.displayDateTime(carryInInfo.receiveTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="上架时间" :value="$util.displayDateTime(carryInInfo.moveTime)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="确认时间" :value="$util.displayDateTime(carryInInfo.finishTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="状态" :value="$util.displayStatus(carryInInfo.status)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="备注" v-model="carryInInfo.remark" hide-details readonly></v-text-field>
-        </v-col>
-      </v-row>
+      <carry-in-details-view :carryInInfo="carryInInfo"> </carry-in-details-view>
     </v-card-text>
 
     <v-card-actions> </v-card-actions>
@@ -90,9 +14,13 @@
 <script>
 import { mapState } from 'vuex'
 import carryIn from '@/controllers/carryIn'
+import CarryInDetailsView from '@/components/View/CarryInDetailsView'
 
 export default {
   name: 'CarryInDetails',
+  components: {
+    CarryInDetailsView
+  },
   data: () => ({
     carryInInfo: {}
   }),
@@ -110,11 +38,6 @@ export default {
   methods: {
     async loadCarryIn() {
       this.carryInInfo = await carryIn.get(this.carryInId)
-    },
-
-    // 显示确认
-    showCarryInFinish() {
-      this.$refs.carryInFinishMod.init(this.carryInInfo.id)
     }
   },
   mounted: function() {
--- src/views/CarryOut/Details.vue
+++ src/views/CarryOut/Details.vue
@@ -4,100 +4,7 @@
       搬运出库任务信息
     </v-card-title>
     <v-card-text>
-      <v-row dense>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="任务码" v-model="carryOutInfo.taskCode" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="搬运类型" :value="$util.carryOutTaskType(carryOutInfo.type)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="仓位码" v-model="carryOutInfo.positionNumber" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="托盘码" v-model="carryOutInfo.trayCode" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="在库数量" v-model="carryOutInfo.storeCount" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="在库重量" v-model="carryOutInfo.storeWeight" suffix="吨" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="移出数量" v-model="carryOutInfo.moveCount" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="移出重量" v-model="carryOutInfo.moveWeight" suffix="吨" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="客户名称" v-model="carryOutInfo.customerName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="合同名称" v-model="carryOutInfo.contractName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="货品名称" v-model="carryOutInfo.cargoName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field
-            label="货品类别"
-            :value="`${carryOutInfo.categoryNumber} - ${carryOutInfo.categoryName}`"
-            hide-details
-            readonly
-          ></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="规格" v-model="carryOutInfo.specification" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="批次" v-model="carryOutInfo.batch" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="产地" v-model="carryOutInfo.originPlace" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="保质期" v-model="carryOutInfo.durability" suffix="月" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="库存入库时间" :value="$util.displayDate(carryOutInfo.inTime)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="库存出库时间" :value="$util.displayDate(carryOutInfo.outTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="接单人" v-model="carryOutInfo.receiveUserName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="接单时间" :value="$util.displayDateTime(carryOutInfo.receiveTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="下架时间" :value="$util.displayDateTime(carryOutInfo.moveTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="清点人" v-model="carryOutInfo.checkUserName" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="清点时间" :value="$util.displayDateTime(carryOutInfo.checkTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="确认时间" :value="$util.displayDateTime(carryOutInfo.finishTime)" hide-details readonly></v-text-field>
-        </v-col>
-
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="状态" :value="$util.displayStatus(carryOutInfo.status)" hide-details readonly></v-text-field>
-        </v-col>
-        <v-col cols="6" sm="6" md="4" lg="2">
-          <v-text-field label="备注" v-model="carryOutInfo.remark" hide-details readonly></v-text-field>
-        </v-col>
-      </v-row>
+      <carry-out-details-view :carryOutInfo="carryOutInfo"> </carry-out-details-view>
     </v-card-text>
 
     <v-card-actions> </v-card-actions>
@@ -107,9 +14,13 @@
 <script>
 import { mapState } from 'vuex'
 import carryOut from '@/controllers/carryOut'
+import CarryOutDetailsView from '@/components/View/CarryOutDetailsView'
 
 export default {
   name: 'CarryOutDetails',
+  components: {
+    CarryOutDetailsView
+  },
   data: () => ({
     carryOutInfo: {}
   }),
