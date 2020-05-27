bb14d5871fd72b481f1cd2f52ebda73e47571819
robertzml <robertzml@126.com>
2020-05-28 00:22:32 +0800

    modify get customer stock flow.

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -5,5 +5,14 @@ export default {
   async getPeriodExpense(model) {
     const res = await axios._post({ url: '/statistic/getPeriodExpense', data: model })
     return res.data
+  },
+
+  // 获取客户出入库流水
+  async getCustomerStockFlow({ customerId, startTime, endTime }) {
+    const res = await axios._get({
+      url: '/statistic/getCustomerStockFlow',
+      params: { customerId: customerId, startTime: startTime, endTime: endTime }
+    })
+    return res.data
   }
 }
--- src/views/Statistic/StockFlow.vue
+++ src/views/Statistic/StockFlow.vue
@@ -12,7 +12,7 @@
     <v-col cols="12">
       <v-card flat>
         <v-card-subtitle class="pb-2 light-blue darken-4">搜索条件</v-card-subtitle>
-        <v-card-text>
+        <v-card-text class="py-0">
           <v-form ref="form" v-model="valid" lazy-validation>
             <v-row dense>
               <v-col cols="3">
@@ -52,10 +52,27 @@
               </v-col>
 
               <v-col cols="2">
-                <v-btn color="success darken-1 mt-2" :disabled="!valid" :loading="loading" @click="searchExpense">搜索</v-btn>
+                <v-btn color="success darken-1 mt-2" :disabled="!valid" :loading="loading" @click="searchStockFlow">搜索</v-btn>
               </v-col>
             </v-row>
           </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12">
+      <v-card flat>
+        <v-card-subtitle class="pb-2 light-green darken-4">过滤条件</v-card-subtitle>
+        <v-card-text class="pt-0">
+          <v-row dense>
+            <v-col cols="4">
+              <v-select :items="$dict.flowType" label="流水类型" v-model="filter.flowType" clearable></v-select>
+            </v-col>
+
+            <v-col cols="4">
+              <v-text-field v-model="filter.text" append-icon="search" label="搜索" clearable single-line hide-details> </v-text-field>
+            </v-col>
+          </v-row>
         </v-card-text>
       </v-card>
     </v-col>
@@ -65,12 +82,14 @@
         <v-card-title class="deep-purple">
           出入库列表
           <v-spacer></v-spacer>
-          <span class="subtitle-2 mr-4">费用合计: 元</span>
         </v-card-title>
         <v-card-text class="px-0">
-          <v-data-table :headers="headers" :items="expenseData" hide-default-footer disable-pagination>
+          <v-data-table :headers="stockFlowHeader" :items="stockFlowFilterData" :search="filter.text" :items-per-page="10">
+            <template v-slot:item.flowDate="{ item }">
+              {{ item.flowDate | displayDate }}
+            </template>
             <template v-slot:item.type="{ item }">
-              {{ item.type | expenseItemType }}
+              {{ item.type | flowType }}
             </template>
           </v-data-table>
         </v-card-text>
@@ -80,6 +99,7 @@
 </template>
 
 <script>
+import statistic from '@/controllers/statistic'
 import CustomerSelect from '@/components/Control/CustomerSelect'
 
 export default {
@@ -96,7 +116,65 @@ export default {
       customerId: 0,
       startTime: null,
       endTime: null
+    },
+    filter: {
+      flowType: 0,
+      text: ''
+    },
+    stockFlowHeader: [
+      { text: '流水日期', value: 'flowDate' },
+      { text: '流水类型', value: 'type' },
+      { text: '流水单号', value: 'flowNumber' },
+      { text: '客户名称', value: 'customerName' },
+      { text: '合同名称', value: 'contractName' },
+      { text: '类别代码', value: 'categoryNumber' },
+      { text: '类别名称', value: 'categoryName' },
+      { text: '货品名称', value: 'cargoName' },
+      { text: '规格', value: 'specification' },
+      { text: '流水数量', value: 'flowCount' },
+      { text: '单位重量(kg)', value: 'unitWeight' },
+      { text: '流水重量(t)', value: 'flowWeight' }
+    ],
+    stockFlowData: []
+  }),
+  computed: {
+    // 过滤后列表
+    stockFlowFilterData() {
+      let temp = this.stockFlowData
+
+      if (this.filter.flowType) {
+        temp = temp.filter(r => r.type == this.filter.flowType)
+      }
+
+      return temp
     }
-  })
+  },
+  methods: {
+    searchStockFlow() {
+      if (this.$refs.form.validate()) {
+        this.$nextTick(() => {
+          this.loading = true
+        })
+        let vm = this
+
+        let model = {
+          customerId: this.search.customerId,
+          startTime: this.search.startTime,
+          endTime: this.search.endTime
+        }
+
+        statistic.getCustomerStockFlow(model).then(res => {
+          vm.stockFlowData = res
+          vm.loading = false
+        })
+      }
+    }
+  },
+  mounted: function() {
+    this.search.startTime = this.$moment()
+      .startOf('month')
+      .format('YYYY-MM-DD')
+    this.search.endTime = this.$moment().format('YYYY-MM-DD')
+  }
 }
 </script>
