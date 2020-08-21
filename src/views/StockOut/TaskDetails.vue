5a087b5cc471e1475b5f953bb725588374d0f97f
robertzml <robertzml@126.com>
2020-08-22 00:18:02 +0800

    add xl breakpoint for StockOutTaskDetailsView, StockOutDetailsView.

--- src/views/StockOut/TaskDetails.vue
+++ src/views/StockOut/TaskDetails.vue
@@ -4,79 +4,14 @@
       <v-expansion-panel>
         <v-expansion-panel-header ripple class="brown darken-1">出库货物信息 </v-expansion-panel-header>
         <v-expansion-panel-content eager>
-          <v-card flat class="mx-auto">
-            <v-row dense>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="$util.displayDate(taskInfo.outTime)" label="出库时间" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.flowNumber" label="出库单流水号" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="$util.stockOutType(taskInfo.stockOutType)" label="出库类型" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.taskCode" label="出库任务码" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.customerName" label="客户名称" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.cargoName" label="货品名称" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="`${taskInfo.categoryNumber} - ${taskInfo.categoryName}`" label="货品类别" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.specification" label="规格" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.unitWeight" label="单位重量" suffix="千克" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.storeCount" label="在库数量" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.outCount" label="出库数量" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.storeWeight" label="在库重量" suffix="吨" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.outWeight" label="出库重量" suffix="吨" hide-details readonly></v-text-field>
-              </v-col>
-
-              <v-col cols="3" md="3" sm="6" v-if="taskInfo.stockOutType == 1">
-                <v-text-field v-model="taskInfo.warehouseName" label="所在仓库" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6" v-if="taskInfo.stockOutType == 1">
-                <v-text-field v-model="taskInfo.place" label="存放位置" hide-details readonly></v-text-field>
-              </v-col>
-
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field v-model="taskInfo.userName" label="创建人" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="$util.displayDateTime(taskInfo.createTime)" label="创建时间" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="$util.displayDateTime(taskInfo.finishTime)" label="完成时间" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="3" md="3" sm="6">
-                <v-text-field :value="$util.displayStatus(taskInfo.status)" label="状态" hide-details readonly></v-text-field>
-              </v-col>
-              <v-col cols="6" md="6" sm="6">
-                <v-text-field v-model="taskInfo.remark" label="备注" hide-details readonly></v-text-field>
-              </v-col>
-            </v-row>
-
-            <v-card-actions>
+          <stock-out-task-details-view :task-info="taskInfo">
+            <template v-slot:action>
               <v-btn color="cyan darken-2" v-if="taskInfo.stockOutType == 2 && taskInfo.status == 81" @click="showScanTray">扫托盘码出库</v-btn>
               <v-btn color="success darken-1" v-if="taskInfo.status == 81" @click.stop="showFinishTask">出库货物确认</v-btn>
               <v-btn color="warning" v-if="taskInfo.stockOutType == 1 && taskInfo.status == 81" @click.stop="showEditTask">编辑出库货物</v-btn>
               <v-btn color="red darken-3" v-if="taskInfo.status != 85" @click.stop="showDeleteTask">删除出库货物</v-btn>
-            </v-card-actions>
-          </v-card>
+            </template>
+          </stock-out-task-details-view>
         </v-expansion-panel-content>
       </v-expansion-panel>
 
@@ -178,6 +113,7 @@ import { mapState, mapMutations, mapActions } from 'vuex'
 import stockOut from '@/controllers/stockOut'
 import carryOut from '@/controllers/carryOut'
 import carryIn from '@/controllers/carryIn'
+import StockOutTaskDetailsView from '@/components/View/StockOutTaskDetailsView'
 import StockOutTaskFinish from '@/components/Dialog/StockOutTaskFinish'
 import StockOutTaskEdit from '@/components/Dialog/StockOutTaskEdit'
 import StockOutTaskDelete from '@/components/Dialog/StockOutTaskDelete'
@@ -195,6 +131,7 @@ export default {
     StockOutTaskFinish,
     StockOutTaskEdit,
     StockOutTaskDelete,
+    StockOutTaskDetailsView,
     ScanTrayOut,
     CarryOutDetails,
     CarryOutDelete,
